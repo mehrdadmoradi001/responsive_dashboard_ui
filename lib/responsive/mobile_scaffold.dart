ef252ef1bcb6741db5534a508139f7e4688ca241
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-03-02 12:50:55 +0330

    1- Creating Drawer 2- Creating constants.dart

--- lib/responsive/mobile_scaffold.dart
+++ lib/responsive/mobile_scaffold.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MobileScaffold extends StatefulWidget {
   const MobileScaffold({Key? key}) : super(key: key);
 
@@ -11,8 +10,35 @@ class MobileScaffold extends StatefulWidget {
 class _MobileScaffoldState extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.green,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[300],
+        child: Column(
+          children: const [
+            DrawerHeader(child: Icon(Icons.favorite)),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('D A S H B O A R D'),
+            ),
+            ListTile(
+              leading: Icon(Icons.chat),
+              title: Text('M E S S A G E'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S E T T I N G S'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('L O G O U T'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
